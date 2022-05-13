0fde0dcb5552884ac7782f31b5b30791c144455a
Konstantin-Dudersky <Konstantin.Dudersky@gmail.com>
2022-05-13 12:47:34 -0700

    network adapters

--- src/src-backend/NetworkAdapter.py
+++ src/src-backend/NetworkAdapter.py
@@ -1,4 +1,3 @@
-
 from dataclasses import dataclass
 
 from pydantic import BaseModel
@@ -14,18 +13,34 @@ class NetworkAdapter(BaseModel):
     config_manager_user_config: bool | None
     creation_class_name: str | None
     description: str | None
-    device_id: str
-    error_cleared: bool
-    error_description: str
-    guid: str
-    index: int
-    install_date: str # datetime
-    installed: bool
-    interface_index: int
-    last_error_code: int
-    mac_address: str
-    manufacturer: str
-    max_number_controlled: int
-    max_speed: int
-    name: str
+    device_id: str | None
+    error_cleared: bool | None
+    error_description: str | None
+    guid: str | None
+    index: int | None
+    install_date: str | None  # datetime
+    installed: bool | None
+    interface_index: int | None
+    last_error_code: int | None
+    mac_address: str | None
+    manufacturer: str | None
+    max_number_controlled: int | None
+    max_speed: int | None
+    name: str | None
     net_connection_id: str | None
+    net_connection_status: int | None
+    net_enabled: bool | None
+    network_addresses: list[str] | None
+    permanent_address: str | None
+    physical_adapter: bool | None
+    pnp_device_id: str | None
+    power_management_capabilities: list[int] | None
+    power_management_supported: bool | None
+    product_name: str | None
+    service_name: str | None
+    speed: int | None
+    status: str | None
+    status_info: int | None
+    system_creation_class_name: str | None
+    system_name: str | None
+    time_of_last_reset: str | None # datetime
--- src/src-backend/main.py
+++ src/src-backend/main.py
@@ -1,4 +1,3 @@
-
 # import subprocess
 
 
@@ -10,26 +9,56 @@ import wmi
 from .NetworkAdapter import NetworkAdapter
 
 # nic_configs = wmi.WMI('').Win32_NetworkAdapterConfiguration(IPEnabled=True)
-nic_configs = wmi.WMI('').Win32_NetworkAdapter()
+nic_configs = wmi.WMI("").Win32_NetworkAdapter()
 
-nics : list[NetworkAdapter] = []
+nics: list[NetworkAdapter] = []
 
 for nic in nic_configs:
     adapter = NetworkAdapter(
-            adapter_type=nic.AdapterType,
-            adapter_type_id=nic.AdapterTypeID,
-            auto_sense=nic.AutoSense,
-            availability=nic.Availability,
-            caption=nic.Caption,
-            net_connection_id=nic.NetConnectionID,
-        )
+        adapter_type=nic.AdapterType,
+        adapter_type_id=nic.AdapterTypeID,
+        auto_sense=nic.AutoSense,
+        availability=nic.Availability,
+        caption=nic.Caption,
+        config_manager_error_code=nic.ConfigManagerErrorCode,
+        config_manager_user_config=nic.ConfigManagerUserConfig,
+        creation_class_name=nic.CreationClassName,
+        description=nic.Description,
+        device_id=nic.DeviceID,
+        error_cleared=nic.ErrorCleared,
+        error_description=nic.ErrorDescription,
+        guid=nic.GUID,
+        index=nic.Index,
+        install_date=nic.InstallDate,
+        installed=nic.Installed,
+        interface_index=nic.InterfaceIndex,
+        last_error_code=nic.LastErrorCode,
+        mac_address=nic.MACAddress,
+        manufacturer=nic.Manufacturer,
+        max_number_controlled=nic.MaxNumberControlled,
+        max_speed=nic.MaxSpeed,
+        name=nic.Name,
+        net_connection_id=nic.NetConnectionID,
+        net_connection_status=nic.NetConnectionStatus,
+        net_enabled=nic.NetEnabled,
+        network_addresses=nic.NetworkAddresses,
+        permanent_address=nic.PermanentAddress,
+        physical_adapter=nic.PhysicalAdapter,
+        pnp_device_id=nic.PNPDeviceID,
+        power_management_capabilities=nic.PowerManagementCapabilities,
+        power_management_supported=nic.PowerManagementSupported,
+        product_name=nic.ProductName,
+        service_name=nic.ServiceName,
+        speed=nic.Speed,
+        status=nic.Status,
+        status_info=nic.StatusInfo,
+        system_creation_class_name=nic.SystemCreationClassName,
+        system_name=nic.SystemName,
+        time_of_last_reset=nic.TimeOfLastReset,
+    )
     print(adapter)
+    print()
     nics.append(adapter)
-
-for nic in nics:
-    print(nic)
-
-
 
 # fastapi
 
@@ -40,17 +69,20 @@ import uvicorn
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*'],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
 @app.get("/", response_model=list[NetworkAdapter])
 async def root():
     return nics
-    
+
 
 if __name__ == "__main__":
-    uvicorn.run(app)
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+    )
